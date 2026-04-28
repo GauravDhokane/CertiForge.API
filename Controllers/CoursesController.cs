be80a583bdfb6173f3a4ea5915dc6148f4040367
Gaurav <gauravdhokane20@gmail.com>
2026-04-29 00:58:18 +0530

    Adding 1 API courses and chanegs in services and models creation

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,25 @@
+﻿using CertiForge.Application.Interfaces.Courses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CertiForge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCoursesAsync()//what is the IActionResult and name of api how it is created 
+        {
+            var courses = await _courseService.GetAllCoursesAsync();
+            return Ok(courses);
+
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 
 using System.Runtime.Intrinsics.X86;
+using CertiForge.Application;
+using CertiForge.Application.Interfaces.Courses;
+using CertiForge.Application.Services;
+using CertiForge.Infrastructure;
 using CertiForge.Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Scalar.AspNetCore;
 
 namespace CertiForge.API
@@ -23,10 +28,29 @@ namespace CertiForge.API
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            //builder.Services.AddAutoMapper(typeof(MappingProfile));
+            // Fix for CS1503: Use the correct overload of AddAutoMapper
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
 
+            builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+            builder.Services.AddScoped<ICourseService, CourseService>();
+
+            //need to chaneg for production
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("default", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+            // configure the http request pipeline
+            app.UseCors("default");
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {

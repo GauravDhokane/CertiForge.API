c586252c00f87db81a1cb1089b8186ca3e5ffef9
Gaurav <gauravdhokane20@gmail.com>
2026-05-05 23:30:02 +0530

    Adding exam interfaces

---
+++ CertiForge.Application/Interfaces/Certification/IExamRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CertiForge.Application.DTOs;
+using CertiForge.Domain.Entities;
+
+namespace CertiForge.Application.Interfaces.Certification
+{
+    public interface IExamRepository
+    {
+        Task<List<Question>> GetRandomQuestionsAsync(int courseId, int count);
+        Task CreateExamWithQuestionsAsync(Exam exam, List<Question> questions);
+        Task UpdateExamQuestionAsync(ExamQuestion examQuestion);
+        Task<ExamQuestion?> GetExamQuestionAsync(int examId, int examQuestionId);
+        Task<List<ExamQuestion>> GetExamQuestionsAsync(int examId);
+        Task<List<UserExam>> GetUserExamsAsync(int userId);
+        Task<ExamDto?> GetExamMetaDataAsync(int examId);
+        Task SaveExamStatusAsync(int examId, string feedback);
+
+        Task<ExamResponseDto> GetExamDetailsAsync(int examId);
+    }
+}

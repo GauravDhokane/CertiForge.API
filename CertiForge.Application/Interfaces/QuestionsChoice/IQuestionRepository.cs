e0119a9d50a6813a9ddf4c5d8ebd8c6640e12295
Gaurav <gauravdhokane20@gmail.com>
2026-05-05 23:39:40 +0530

    Adding questionrepo and services to app

---
+++ CertiForge.Application/Interfaces/QuestionsChoice/IQuestionRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CertiForge.Application.DTOs;
+using CertiForge.Domain.Entities;
+
+namespace CertiForge.Application.Interfaces.QuestionsChoice
+{
+    public interface IQuestionRepository
+    {
+        Task<IEnumerable<Question>> GetAllQuestionsAsync();
+        Task<Question?> GetQuestionByIdAsync(int id);
+        Task<Question> AddQuestionAsync(Question question);
+        Task UpdateQuestionAsync(Question question);
+        Task DeleteQuestionAsync(Question question);
+        Task UpdateQuestionAndChoicesAsync(int id, QuestionDto dto);
+    }
+}
---
+++ CertiForge.Application/Interfaces/QuestionsChoice/IQuestionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CertiForge.Application.DTOs;
+
+namespace CertiForge.Application.Interfaces.QuestionsChoice
+{
+    public interface IQuestionService
+    {
+        Task<IEnumerable<QuestionDto>> GetAllQuestionsAsync();
+        Task<QuestionDto?> GetQuestionByIdAsync(int id);
+        Task AddQuestionAsync(CreateQuestionDto dto);
+        Task UpdateQuestionAsync(int id, UpdateQuestionDto dto);
+        Task DeleteQuestionAsync(int id);
+        Task<QuestionDto> AddQuestionAndChoicesAsync(QuestionDto dto);
+        Task UpdateQuestionAndChoicesAsync(int id, QuestionDto dto);
+    }
+}
